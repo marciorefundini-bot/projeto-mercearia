cc29bdbc3add65b00f81cffa0f8c4c1bd3795e7f
Marcio <marciorefundini@gmail.com>
2026-03-13 23:00:08 -0300

    Sistema de fiado com total da divida por cliente

--- mercearia/clientes/admin.py
+++ mercearia/clientes/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import Cliente, Produto, Fiado
 
 
+class FiadoInline(admin.TabularInline):
+    model = Fiado
+    extra = 0
+
+
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ("nome", "telefone", "divida_total")
+    readonly_fields = ("divida_total",)
+    inlines = [FiadoInline]
 
 
 @admin.register(Produto)
