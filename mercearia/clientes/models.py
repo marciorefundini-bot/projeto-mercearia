833ce1c5aa1ea766bd8a5332046f2cd681e1f6cd
Marcio <marciorefundini@gmail.com>
2026-03-13 21:29:23 -0300

    feat: cadastro de produtos com preço

--- mercearia/clientes/models.py
+++ mercearia/clientes/models.py
@@ -7,3 +7,11 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.nome  
